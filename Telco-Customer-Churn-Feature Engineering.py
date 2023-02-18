d11df17fefd326c08514679836d9005dca0b758e
Furkan Deveci <114950642+MFurkanDeveci@users.noreply.github.com>
2023-02-18 15:05:41 +0300

    Create Telco-Customer-Churn-Feature Engineering.py

---
+++ Telco-Customer-Churn-Feature Engineering.py
@@ -0,0 +1,417 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+# !pip install pandas plotnine
+from plotnine import ggplot, aes, geom_col, geom_histogram, geom_bar, geom_point, geom_boxplot, geom_line, geom_col, \
+    geom_boxplot, geom_density
+# !pip install missingno
+import missingno as msno
+from datetime import date
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, RobustScaler
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+
+df = pd.read_csv("Telco-Customer-Churn.csv")
+df.head()
+df.info()
+
+# Görev 1 : Keşifçi Veri Analizi
+# Adım 1: Genel resmi inceleyiniz.
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.describe([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+    print("##################### essiz deger sayisi #####################")
+    print(dataframe.nunique())
+check_df(df)
+
+# TotalCharges değişkeni object tipinden float tipine dönüştürüldü.
+df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors='coerce')
+df["TotalCharges"].head()
+# Adım 2: Numerik ve kategorik değişkenleri yakalayınız.
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+# Adım 3: Numerik ve kategorik değişkenlerin analizini yapınız.
+
+# Numerik değişken analizi;
+
+def num_summary(dataframe, numerical_col, plot = False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist()
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+df.describe().T
+
+# kategorik değişken analizi;
+df.info()
+
+def cat_summary(dataframe, col_name, plot = False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+cat_summary(df, "gender", plot = False)
+
+for col in cat_cols:
+    if df[col].dtypes == "bool":
+        print("###########")
+    else:
+        cat_summary(df, col, plot=True)
+
+# Adım 4: Hedef değişken analizi yapınız. (Kategorik değişkenlere göre hedef değişkenin ortalaması, hedef değişkene göre
+# numerik değişkenlerin ortalaması)
+# kategorik değişkenlere göre hedef değişken analizi;
+
+# kategorik değişkenin içinde ki değişkenleri herhangi bir değer ile değiştirmek istersek;
+df["Churn"] = df["Churn"].map({'No': 0, 'Yes': 1})
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"Churn": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+
+target_summary_with_cat(df, "Churn", "Partner")
+
+for col in cat_cols:
+    target_summary_with_cat(df, "Churn", col)
+
+
+
+# sayısal değişkenlere göre hedef değişken analizi;
+
+num_cols
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+target_summary_with_num(df, "Churn", "tenure")
+
+for col in num_cols:
+    target_summary_with_num(df, "Churn", col)
+
+
+# Adım 5: Aykırı gözlem analizi yapınız.
+num_cols
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+low, up = outlier_thresholds(df, "tenure")
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+check_outlier(df, "tenure")
+check_outlier(df, "MonthlyCharges")
+check_outlier(df, "TotalCharges")
+# Adım 6: Eksik gözlem analizi yapınız.
+df.isnull().sum()
+
+# TotalCharges değişkenindeki eksik değerler median ile doldurulacak.
+df["TotalCharges"].median()
+df["TotalCharges"].mean()
+df["TotalCharges"] = df["TotalCharges"].fillna(df["TotalCharges"].median())
+df["TotalCharges"].head()
+df.isnull().sum()
+# Adım 7: Korelasyon analizi yapınız.
+num_cols
+corr = df[num_cols].corr()
+corr
+
+sns.set(rc={'figure.figsize': (6,6)})
+sns.heatmap(corr, cmap="RdBu")
+plt.show(block=True)
+
+# farklı bir şekildeki korelasyon matrisi
+f, ax = plt.subplots(figsize=[18, 13])
+sns.heatmap(df[num_cols].corr(), annot=True, fmt=".2f", ax=ax, cmap="magma")
+ax.set_title("Correlation Matrix", fontsize=20)
+plt.show()
+
+##########################################################################
+# Değişken oluşturmadan Base Model Kurulumu
+
+# One-Hot Encoder İşlemi
+dff = df.copy()
+cat_cols = [col for col in cat_cols if col not in ["Churn"]]
+cat_cols
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+dff = one_hot_encoder(dff, cat_cols, drop_first=True)
+
+dff.head()
+dff.isnull().sum()
+# Model Kurulumu
+
+from sklearn.ensemble import RandomForestClassifier
+
+y = dff["Churn"]
+X = dff.drop(["customerID", "Churn"], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+rf_model = RandomForestClassifier(random_state=46).fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+accuracy_score(y_pred, y_test)
+
+# Model başarı metrikleri
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,roc_auc_score
+
+print(f"Accuracy: {round(accuracy_score(y_pred, y_test), 4)}")
+print(f"Recall: {round(recall_score(y_pred,y_test),4)}")
+print(f"Precision: {round(precision_score(y_pred,y_test), 4)}")
+print(f"F1: {round(f1_score(y_pred,y_test), 4)}")
+print(f"Auc: {round(roc_auc_score(y_pred,y_test), 4)}")
+
+def plot_importance(model, features, num=len(X), save=False):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
+                                                                      ascending=False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+    if save:
+        plt.savefig('importances.png')
+
+plot_importance(rf_model, X_train)
+
+# Encdoing İşlemleri
+
+le = LabelEncoder()
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+
+binary_cols = [col for col in df.columns if df[col].dtype not in ["int64", "float64"] and df[col].nunique() == 2]
+
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+df.head()
+
+
+
+#############################################################################
+
+# Görev 2 : Feature Engineering
+
+# Adım 1: Eksik ve aykırı gözlemler için gerekli işlemleri yapınız.
+df.isnull().sum()
+
+# eksik değerlerimiz ve aykırı gözlemlerimiz yok.
+# Adım 2: Yeni değişkenler oluşturunuz.
+
+# Tenure  değişkeninden yıllık kategorik değişken oluşturma
+df.loc[(df["tenure"]>=0) & (df["tenure"]<=12),"NEW_TENURE_YEAR"] = "0-1 Year"
+df.loc[(df["tenure"]>12) & (df["tenure"]<=24),"NEW_TENURE_YEAR"] = "1-2 Year"
+df.loc[(df["tenure"]>24) & (df["tenure"]<=36),"NEW_TENURE_YEAR"] = "2-3 Year"
+df.loc[(df["tenure"]>36) & (df["tenure"]<=48),"NEW_TENURE_YEAR"] = "3-4 Year"
+df.loc[(df["tenure"]>48) & (df["tenure"]<=60),"NEW_TENURE_YEAR"] = "4-5 Year"
+df.loc[(df["tenure"]>60) & (df["tenure"]<=72),"NEW_TENURE_YEAR"] = "5-6 Year"
+
+# Kontratı 1 veya 2 yıllık müşterileri Engaged olarak belirtme
+df["NEW_Engaged"] = df["Contract"].apply(lambda x: 1 if x in ["One year","Two year"] else 0)
+
+# Herhangi bir destek, yedek veya koruma almayan kişiler
+df["NEW_noProt"] = df.apply(lambda x: 1 if (x["OnlineBackup"] != "Yes") or (x["DeviceProtection"] != "Yes") or (x["TechSupport"] != "Yes") else 0, axis=1)
+
+# Aylık sözleşmesi bulunan ve genç olan müşteriler
+df["NEW_Young_Not_Engaged"] = df.apply(lambda x: 1 if (x["NEW_Engaged"] == 0) and (x["SeniorCitizen"] == 0) else 0, axis=1)
+
+
+# Kişinin toplam aldığı servis sayısı
+df['NEW_TotalServices'] = (df[['PhoneService', 'InternetService', 'OnlineSecurity',
+                                       'OnlineBackup', 'DeviceProtection', 'TechSupport',
+                                       'StreamingTV', 'StreamingMovies']]== 'Yes').sum(axis=1)
+
+
+# Herhangi bir streaming hizmeti alan kişiler
+df["NEW_FLAG_ANY_STREAMING"] = df.apply(lambda x: 1 if (x["StreamingTV"] == "Yes") or (x["StreamingMovies"] == "Yes") else 0, axis=1)
+
+# Kişi otomatik ödeme yapıyor mu?
+df["NEW_FLAG_AutoPayment"] = df["PaymentMethod"].apply(lambda x: 1 if x in ["Bank transfer (automatic)","Credit card (automatic)"] else 0)
+
+# ortalama aylık ödeme
+df["NEW_AVG_Charges"] = df["TotalCharges"] / (df["tenure"] + 1)
+
+# Güncel Fiyatın ortalama fiyata göre artışı
+df["NEW_Increase"] = df["NEW_AVG_Charges"] / df["MonthlyCharges"]
+
+# Servis başına ücret
+df["NEW_AVG_Service_Fee"] = df["MonthlyCharges"] / (df['NEW_TotalServices'] + 1)
+
+
+# Adım 3: Encoding işlemlerini gerçekleştiriniz.
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+binary_cols = [col for col in df.columns if df[col].dtypes == "O" and df[col].nunique() == 2]
+binary_cols
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+df.head()
+cat_cols = [col for col in cat_cols if col not in binary_cols and col not in ["Churn", "NEW_TotalServices"]]
+cat_cols
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+
+df.head()
+
+# Adım 4: Numerik değişkenler için standartlaştırma yapınız.
+
+scaler = StandardScaler()
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+df[num_cols].head()
+
+
+# Adım 5: Model oluşturunuz.
+
+y = df["Churn"]
+X = df.drop(["customerID", "Churn"], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+rf_model = RandomForestClassifier(random_state=46).fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+accuracy_score(y_pred, y_test)
+
+# Model başarı metrikleri
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,roc_auc_score
+
+print(f"Accuracy: {round(accuracy_score(y_pred, y_test), 4)}")
+print(f"Recall: {round(recall_score(y_pred,y_test),4)}")
+print(f"Precision: {round(precision_score(y_pred,y_test), 4)}")
+print(f"F1: {round(f1_score(y_pred,y_test), 4)}")
+print(f"Auc: {round(roc_auc_score(y_pred,y_test), 4)}")
+
+def plot_importance(model, features, num=len(X), save=False):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
+                                                                      ascending=False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+    if save:
+        plt.savefig('importances.png')
+
+plot_importance(rf_model, X_train)
+
+# Değişken Üretilen Model     Base Model
+#Accuracy: 0.7861             Accuracy: 0.7894
+#Recall: 0.6406               Recall: 0.6503
+#Precision: 0.4843            Precision: 0.4861
+#F1: 0.5516                   F1: 0.5563
+#Auc: 0.7321                  Auc: 0.7376
+
